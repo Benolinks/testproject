4fe4f4a7e846194216788ffb6b7a1ca9d0030eef
Benolinks <benolinks1@gmail.com>
2022-09-01 14:07:33 +0100

    to commit toggle test folder

---
+++ app.js
@@ -0,0 +1,9 @@
+var toggle_two = document.getElementById("toggle-two")
+var toggle_three = document.getElementById("toggle-three")
+var toggle_one = document.getElementById("toggle-one")
+
+
+
+function clicks() {
+    toggle_three.classList.toggle("toggleones")
+}
